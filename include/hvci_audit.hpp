a32e52d9274996f5de78b815bd7bc356e0ed9254
Urjosh123 <gowerjosh135@gmail.com>
2026-04-01 20:34:05 +0100

    eac kernel

---
+++ include/hvci_audit.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+
+namespace hvci_audit
+{
+	struct SlatEntry
+	{
+		uint64_t virtual_addr;
+		uint64_t physical_addr;
+		uint64_t entry_bits;
+	};
+
+	bool PerformGhostWalk(uintptr_t host_base, uint32_t host_size);
+	uint64_t FindSlatBase();
+	bool ValidateConsistency(uint64_t entry);
+}
