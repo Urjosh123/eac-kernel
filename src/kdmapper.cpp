290b4aac87fb92d25b50054b0057d372bb1f3cce
Urjosh123 <gowerjosh135@gmail.com>
2026-04-01 21:31:15 +0100

    eac kernel

--- src/kdmapper.cpp
+++ src/kdmapper.cpp
@@ -80,14 +80,65 @@ bool kdmapper::MapDriver(HANDLE iqvw64e_device_handle, const std::string& driver
 
 	intel_driver::ExecuteViaIPI(iqvw64e_device_handle, (uint64_t)target_base + pe_file.entry_point);
 
-	bool kdmapper::Relocate(uint8_t* raw_image, uint64_t target_base, uint64_t source_base)
+	bool Relocate(uint8_t* raw_image, uint64_t target_base, uint64_t source_base)
 	{
-		std::cout << "[+] PE Relocation: Applying base offsets for 2026 platform..." << std::endl;
+		uint64_t delta = target_base - source_base;
+		if (delta == 0) return true;
+
+		PIMAGE_DOS_HEADER dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(raw_image);
+		PIMAGE_NT_HEADERS64 nt_headers = reinterpret_cast<PIMAGE_NT_HEADERS64>(raw_image + dos_header->e_lfanew);
+		PIMAGE_DATA_DIRECTORY reloc_dir = &nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+		
+		if (reloc_dir->Size == 0) return true;
+
+		PIMAGE_BASE_RELOCATION reloc = reinterpret_cast<PIMAGE_BASE_RELOCATION>(raw_image + reloc_dir->VirtualAddress);
+		while (reloc->VirtualAddress != 0)
+		{
+			uint32_t size = (reloc->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(uint16_t);
+			uint16_t* list = reinterpret_cast<uint16_t*>(reloc + 1);
+			for (uint32_t i = 0; i < size; i++)
+			{
+				if ((list[i] >> 12) == IMAGE_REL_BASED_DIR64)
+				{
+					uint64_t* ptr = reinterpret_cast<uint64_t*>(raw_image + reloc->VirtualAddress + (list[i] & 0xFFF));
+					*ptr += delta;
+				}
+			}
+			reloc = reinterpret_cast<PIMAGE_BASE_RELOCATION>(reinterpret_cast<uint8_t*>(reloc) + reloc->SizeOfBlock);
+		}
+		
+		std::cout << "[+] PE Relocation: Base offsets applied." << std::endl;
 		return true;
 	}
 
-	bool kdmapper::ResolveImports(HANDLE iqvw64e_device_handle, uint8_t* raw_image)
+	bool ResolveImports(HANDLE iqvw64e_device_handle, uint8_t* raw_image)
 	{
-		std::cout << "[+] IAT Resolver: Linking kernel exports (ntoskrnl.exe, hal.dll, ci.dll)..." << std::endl;
+		PIMAGE_DOS_HEADER dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>(raw_image);
+		PIMAGE_NT_HEADERS64 nt_headers = reinterpret_cast<PIMAGE_NT_HEADERS64>(raw_image + dos_header->e_lfanew);
+		PIMAGE_DATA_DIRECTORY import_dir = &nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+
+		if (import_dir->Size == 0) return true;
+
+		PIMAGE_IMPORT_DESCRIPTOR import_desc = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(raw_image + import_dir->VirtualAddress);
+		while (import_desc->Name != 0)
+		{
+			const char* module_name = reinterpret_cast<const char*>(raw_image + import_desc->Name);
+			uint64_t module_base = utils::GetKernelModuleBase(module_name);
+			if (module_base == 0) return false;
+
+			PIMAGE_THUNK_DATA64 thunk = reinterpret_cast<PIMAGE_THUNK_DATA64>(raw_image + import_desc->FirstThunk);
+			while (thunk->u1.AddressOfData != 0)
+			{
+				PIMAGE_IMPORT_BY_NAME import_by_name = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(raw_image + thunk->u1.AddressOfData);
+				uint64_t func_addr = utils::GetKernelExport(module_base, import_by_name->Name);
+				if (func_addr == 0) return false;
+				thunk->u1.Function = func_addr;
+				thunk++;
+			}
+			import_desc++;
+		}
+
+		std::cout << "[+] IAT Resolver: Kernel imports linked." << std::endl;
 		return true;
 	}
+}
